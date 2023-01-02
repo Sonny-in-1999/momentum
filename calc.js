241e2a3bf33215f13f00a031b367546213a51e5c
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-02 14:47:50 +0900

    made calc

---
+++ calc.js
@@ -0,0 +1,23 @@
+const calculator = {
+    plus: function(a,b) {
+        return a+b;
+    },
+    minus: function(a,b) {
+        return a-b;
+    },
+    times: function(a,b) {
+        return a*b;
+    },
+    divide: function(a,b) {
+        return a/b;
+    },
+    power: function(a,b) {
+        return a**b;
+    }
+};
+
+console.log(calculator.plus(2,3));
+console.log(calculator.minus(2,3));
+console.log(calculator.times(2,3));
+console.log(calculator.devide(2,3));
+console.log(calculator.power(2,3));
