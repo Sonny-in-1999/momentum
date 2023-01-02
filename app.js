ba0918859336cbb4260597d68bd059d9ce8058dd
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-02 15:32:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ if (isNaN(age)) {
 else if (age < 18) {
     alert("You are too young to drink");
 }
-else{
-    alert("Welcome!")
+else if (age >= 18 && age < 50) {
+    alert("Welcome! Enjoy your time.");
 }
+else if (age >= 50 && age < 70) {
+    alert("Sir. You should do some exercise rather than drink. ");
+}
+else if (age >= 70 && age < 80) {
+    alert("Damn it. You are too old.")
+}
+else if (age >= 80 && age < 99) {
+    alert("Still alive?")
+}
+else if (age == 100) {
+    alert("Wow....")
+}
+
+
 //parseInt: make str to int
 //typeof: return type of the var
 //isNan return true if the var is num(false if the var isn't num)
