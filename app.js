cbf16cb12a4aec734c5279c576fc6e6c35ea66cd
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-02 11:15:12 +0900

    Learning function pt,2
    You can put function inside array.

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-function sayHello(name){
-    console.log("Hello my name is " + name + "!");
+const player = {
+    name: "minsu",
+    age: 25,
+    live: true,
+    sayHello: function (name) {
+        console.log("Hello, " + name + "!");
+    }
 }
 
-sayHello("Python");
-sayHello("Java");
-sayHello("Django");
-sayHello("Ruby");
-sayHello("Go");
-
-
-function nameWithAge(a, b){
-    console.log("My name is " + a + " and " + "I am " + b + " years old!");
-}
-
-nameWithAge("Minsu", 25);
-nameWithAge("HyungHoon", 24);
-
-function plus(fNum, sNum){
-    console.log(fNum+sNum);
-}
-
-plus(20, 30);
-plus(30, 70);
+console.log(player.sayHello("Dave"));
