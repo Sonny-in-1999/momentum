bae7274a311a7cb223da3c099331e03eec8e4f95
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-02 10:53:54 +0900

    Learning Object
    Const로 선언해도 Object의 개체는 수정이 가능하며 새로운 개체를 추가할 수도 있음. Object 자체를 수정하는 것은 불가능함.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const playerInfo = { //property
+    name:"minsu",
+    age: 25,
+    live: true
+};
 
+console.log(playerInfo);
+console.log(playerInfo.age);
 
+playerInfo.live = false;
+console.log(playerInfo);
 
+playerInfo.lastName = "son";
+console.log(playerInfo); 
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-
-console.log(daysOfWeek); //array(7)
-
-console.log(daysOfWeek[4]); //fri
-
-daysOfWeek.push("num");
-
-console.log(daysOfWeek);
+playerInfo.age += 10;
+console.log(playerInfo.age);
