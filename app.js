145343848d541c81c00d37871592dcbb77e732cd
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-02 15:09:32 +0900

    Check age

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const age = prompt("How old are you?");
 
 if (age <= 18) {
-    alert("You are FUCKING YOUNG to drink. FUCK OFF.");
+    alert("You are too young to drink. Get out.");
+}
+if (isNaN(age)) {
+    alert("DD NOT FOOL AROUND.")
 }
 else{
     alert("Welcome!")
 }
-
-//parseInt make str to int
+//parseInt: make str to int
+//typeof: return type of the var
+//isNan return true if the var is num(false if the var isn't num)
 
 console.log(age);
 console.log(parseInt(age));
 
 console.log(typeof 15);
 console.log(typeof parseInt(age));
+
+console.log(isNaN(age));
